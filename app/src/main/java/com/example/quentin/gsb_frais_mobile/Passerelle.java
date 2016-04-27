4953a5928194a03ad4afc5f8b257b208ccdef453
Quentin DEL MISTRO <quentin.delmistro@epsi.fr>
2016-04-27 15:51:19 +0200

    Ajout Getters et Setters classe Passerelle

--- app/src/main/java/com/example/quentin/gsb_frais_mobile/Passerelle.java
+++ app/src/main/java/com/example/quentin/gsb_frais_mobile/Passerelle.java
@@ -1,8 +1,24 @@
 package com.example.quentin.gsb_frais_mobile;
 
-/**
- * Created by Quentin on 27/04/2016.
- */
 public class Passerelle {
-    
+    private double coutDieselCV4 = 0.52;
+    private double coutDieselCV5Et6 = 0.56;
+    private double coutEssenceCV4 = 0.56;
+    private double coutEssenceCV5Et6 = 0.58;
+
+    public double GetCoutDieselCV4() {
+        return coutDieselCV4;
+    }
+
+    public double GetCoutDieselCV5Et6() {
+        return coutDieselCV5Et6;
+    }
+
+    public double GetCoutEssenceCV4() {
+        return coutEssenceCV4;
+    }
+
+    public double GetCoutEssenceCV5Et6() {
+        return coutEssenceCV5Et6;
+    }
 }
