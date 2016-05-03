6abbb117c647459afe84c213b4f6d84bc9c469ba
Quentin DEL MISTRO <quentin.delmistro@epsi.fr>
2016-05-03 13:59:02 +0200

    synchro git ignore 1

--- app/src/main/java/com/example/quentin/gsb_frais_mobile/Passerelle.java
+++ app/src/main/java/com/example/quentin/gsb_frais_mobile/Passerelle.java
@@ -1,5 +1,7 @@
 package com.example.quentin.gsb_frais_mobile;
 
+import java.net.URL;
+
 public class Passerelle {
     private double coutDieselCV4 = 0.52;
     private double coutDieselCV5Et6 = 0.56;
@@ -21,4 +23,9 @@ public class Passerelle {
     public double GetCoutEssenceCV5Et6() {
         return coutEssenceCV5Et6;
     }
+
+    public void dbConnection(){
+
+    }
+
 }
