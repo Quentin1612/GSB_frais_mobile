dad0e761ffc858b9c878f2a1fb2b8124a4117cf9
Quentin DEL MISTRO <quentin.delmistro@epsi.fr>
2016-05-25 08:30:00 +0200

    affichage / traitement terminé

--- app/src/main/java/com/example/quentin/gsb_frais_mobile/MainActivity.java
+++ app/src/main/java/com/example/quentin/gsb_frais_mobile/MainActivity.java
@@ -1,25 +1,20 @@
 package com.example.quentin.gsb_frais_mobile;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
